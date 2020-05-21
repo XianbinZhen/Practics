924e448196b05578732645bf8d00d79aab45add0
GIT Admin <xianbin.zhen@umb.edu>
2020-05-21 12:20:09 -0400

    Look and say sequence

--- LookAndSaySequence.py
+++ LookAndSaySequence.py
@@ -10,3 +10,38 @@ Each consecutive value describes the prior value.
 
 Your task is, return the nth term of this sequence.
 """
+
+
+def look_and_say_seq(nth):
+    sequence = [1]
+    for n in range(1, nth + 1):
+        sequence = calculate_sequence([], sequence)
+    return sequence
+
+
+def calculate_sequence(preq, sequence):
+    if sequence:
+        print("sequence is: ", sequence)
+        length = len(sequence)
+        start = sequence[0]
+        count = 1
+        for n in range(1, length):
+            if start != sequence[n]:
+                count = n
+                print("start is: ", start, "count is: ", count)
+                break
+            else:
+                count += 1
+        preq.append(count)
+        preq.append(start)
+
+        print("preq is: ", preq)
+        return calculate_sequence(preq, sequence[count:])
+    else:
+        return preq
+
+#
+# seq1 = []
+# seq2 = [1, 2, 1, 1]
+# print(calculate_sequence(seq1, seq2))
+print(look_and_say_seq(5))
